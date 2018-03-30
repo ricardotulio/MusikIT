33ad8ddc3ae02588a0238ad28a78433d47816c8c
Morkhusz <jose-silvafilho@hotmail.com>
2018-03-30 11:28:54 -0300

    browser test and laravel modules dependencies added

---
+++ tests/Browser/Login/LoginTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Browser\Login;
+
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    public function testShouldSuccessfullyLogin()
+    {
+        $this->browse(function (Browser $browser) {
+
+        });
+    }
+}
